691bf0d91b16d2ab882df992ee1ba6f5f3ebe86e
KhaiPv <khaipv@piontech.co>
2024-09-18 16:59:54 +0700

    Need delete: drawTriangle on gles20

--- app/src/main/java/com/demo/fluid/util/gl/GLES20RendererLWP.java
+++ app/src/main/java/com/demo/fluid/util/gl/GLES20RendererLWP.java
@@ -11,6 +11,11 @@ import android.view.SurfaceHolder;
 
 import com.magicfluids.MotionEventWrapper;
 import com.magicfluids.NativeInterface;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -29,10 +34,30 @@ public final class GLES20RendererLWP implements GLSurfaceView.Renderer {
     private int screenWidth;
 
     private Bitmap imageBitmap;
-    public GLES20RendererLWP(NativeInterface nativeInterface, OrientationSensor orientationSensor,Bitmap bitmap) {
+
+    private int shaderProgram;
+    private int positionHandle;
+    private int colorHandle;
+    private FloatBuffer vertexBuffer;
+
+    private float[] triangleVertices = {
+            0.0f,  0.5f, 0.0f,  // Đỉnh trên cùng
+            -0.5f, -0.5f, 0.0f,  // Đỉnh trái
+            0.5f, -0.5f, 0.0f   // Đỉnh phải
+    };
+
+
+    public GLES20RendererLWP(NativeInterface nativeInterface, OrientationSensor orientationSensor, Bitmap bitmap) {
         this.nativeInterface = nativeInterface;
         this.orientationSensor = orientationSensor;
         this.imageBitmap = bitmap;
+
+        // Trong constructor
+        ByteBuffer bb = ByteBuffer.allocateDirect(triangleVertices.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(triangleVertices);
+        vertexBuffer.position(0);
     }
 
     public NativeInterface getNativeInterface() {
@@ -50,6 +75,45 @@ public final class GLES20RendererLWP implements GLSurfaceView.Renderer {
     public void setInitialScreenSize(int i, int i2) {
         this.screenWidth = i;
         this.screenHeight = i2;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eGLConfig) {
+        if (!isLWP()) {
+            checkGPUExtensions();
+            onEGLContextCreated();
+        }
+
+        // Tạo shader và chương trình shader
+        String vertexShaderCode =
+                "attribute vec4 vPosition;" +
+                        "void main() {" +
+                        "  gl_Position = vPosition;" +
+                        "}";
+
+        String fragmentShaderCode =
+                "precision mediump float;" +
+                        "uniform vec4 uColor;" +
+                        "void main() {" +
+                        "  gl_FragColor = uColor;" +
+                        "}";
+
+        int vertexShader = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
+        GLES20.glShaderSource(vertexShader, vertexShaderCode);
+        GLES20.glCompileShader(vertexShader);
+
+        int fragmentShader = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
+        GLES20.glShaderSource(fragmentShader, fragmentShaderCode);
+        GLES20.glCompileShader(fragmentShader);
+
+        shaderProgram = GLES20.glCreateProgram();
+        GLES20.glAttachShader(shaderProgram, vertexShader);
+        GLES20.glAttachShader(shaderProgram, fragmentShader);
+        GLES20.glLinkProgram(shaderProgram);
+
+        // Xác định các handles
+        positionHandle = GLES20.glGetAttribLocation(shaderProgram, "vPosition");
+        colorHandle = GLES20.glGetUniformLocation(shaderProgram, "uColor");
     }
 
     @Override
@@ -80,15 +144,23 @@ public final class GLES20RendererLWP implements GLSurfaceView.Renderer {
         }
         this.nativeInterface.updateApp(this.ignoreNextFrameTime, false, this.orientationSensor.getAccelerationX(), this.orientationSensor.getAccelerationY(), this.orientationSensor.getOrientation());
         this.ignoreNextFrameTime = false;
+        drawTriangle();
     }
 
-    @Override
-    public void onSurfaceCreated(GL10 gl10, EGLConfig eGLConfig) {
-        if (!isLWP()) {
-            checkGPUExtensions();
-            onEGLContextCreated();
-        }
+    private void drawTriangle() {
+        GLES20.glUseProgram(shaderProgram);
+
+        vertexBuffer.position(0);
+        GLES20.glEnableVertexAttribArray(positionHandle);
+        GLES20.glVertexAttribPointer(positionHandle, 3, GLES20.GL_FLOAT, false, 0, vertexBuffer);
+
+        GLES20.glUniform4f(colorHandle, 1.0f, 1.0f, 1.0f, 1.0f); // Màu trắng
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+
+        GLES20.glDisableVertexAttribArray(positionHandle);
     }
+
+
 
     @Override
     public void onSurfaceChanged(GL10 gl10, int i, int i2) {
@@ -106,3 +178,4 @@ public final class GLES20RendererLWP implements GLSurfaceView.Renderer {
         this.nativeInterface.onGLContextRestarted();
     }
 }
+
